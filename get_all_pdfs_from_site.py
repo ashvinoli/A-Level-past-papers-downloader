61e22e1b80eed9877bb24cc4ee86391251942d62
Ashvin Oli <40764714+ashvinoli@users.noreply.github.com>
2022-09-18 18:48:06 +0545

    urllib.join not working fixed. Merge pdfs work for all directories without putting it inside all

--- get_all_pdfs_from_site.py
+++ get_all_pdfs_from_site.py
@@ -2,88 +2,42 @@ import os
 import requests
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
-import re
-import pathlib #this library lets mkdir word recursively, default mkdir could only crease folders to one level up
+import pathlib 
+from datetime import date
+import json
 
-url = "http://onlineexamhelp.com/past-papers/cambridge-international-a-as-level"
 
-
-def form_dic():
-    global url
-    my_dict = {}
-    soup = BeautifulSoup(requests.get(url).text,"html.parser")
-    all_html = soup.findAll('a')
-    level = 1
-    for _ in all_html:
-        my_href = _.get("href").split('/')[-1]
-        if re.match("^\d+",my_href):
-            my_dict[level] = my_href
-            level+=1
-    return my_dict 
-
-def download_all_from_extreme_papers():
+def download_all():
     dir_path = os.path.dirname(os.path.realpath(__file__))   
-    urls = ["https://papers.xtremepape.rs/CAIE/AS and A Level/Biology (9700)/","https://papers.xtremepape.rs/CAIE/AS and A Level/Mathematics (9709)/","https://papers.xtremepape.rs/CAIE/AS and A Level/Physics (9702)/","https://papers.xtremepape.rs/CAIE/AS and A Level/Computer Science (9608)/","https://papers.xtremepape.rs/CAIE/AS and A Level/Chemistry (9701)/"]
-    count = 0
-    for url in urls:
-        soup = BeautifulSoup(requests.get(url).text,"html.parser")
-        folder_location = os.path.join(dir_path,url.split("/")[-2])
-        if not os.path.exists(folder_location):
-            pathlib.Path(folder_location).mkdir(parents=True, exist_ok=True)
-        all_html = soup.findAll('a')
-        for links in all_html:
-            my_href = str(links.get("href"))
-            if my_href.endswith(".pdf"):
-                print("Downloading file %s"%(my_href))
-                file_name = os.path.join(folder_location,my_href)
-                if not os.path.exists(file_name):
-                    my_file = open(file_name,"wb")
-                    my_file.write(requests.get(urljoin(url,my_href)).content)
-                    my_file.close()
+    with open("subjects.json") as f:
+        data = json.load(f)
+        url_header = data["site"]
+        subjects = data["subjects"]
+        combined_urls = [urljoin(url_header,sub) for sub in subjects]
+        for url in combined_urls:
+            cur_year = date.today().year
+            init_year = data["init_year"]
+            for year in range(init_year,cur_year+1):
+                final_url = f"{url}/{year}"
+                soup = BeautifulSoup(requests.get(final_url).text,"html.parser")
+                folder_location = os.path.join(dir_path,final_url.split("/")[-2])
+                if not os.path.exists(folder_location):
+                    pathlib.Path(folder_location).mkdir(parents=True, exist_ok=True)
+                all_html = soup.findAll('a')
+                for links in all_html:
+                    my_href = str(links.get("href"))
+                    if my_href.endswith(".pdf"):
+                        print("Downloading file %s"%(my_href))
+                        file_name = os.path.join(folder_location,my_href)
+                        if not os.path.exists(file_name):
+                            my_file = open(file_name,"wb")
+                            download_link = f"{final_url}/{my_href}"
+                            my_file.write(requests.get(download_link).content)
+                            my_file.close() 
+            
                 
-                
-def download_papers(my_sub,url):
-    complete_url = url+"/"+my_sub
-    soup = BeautifulSoup(requests.get(complete_url).text,"html.parser")
-    all_html = soup.findAll('a')
-    available_years = []
-    for _ in all_html:
-        my_href = _.get("href").split('/')[-1]
-        if re.match("^\d+.*\d$",my_href):
-            av_year = my_href.split("-")[-1]
-            available_years.append(av_year)                         
-            sub_name = my_href
-            sub_name = sub_name[:len(my_href)-4]
-    print("Avalilable Years:" + ",".join(available_years))
-    start_year = int(input("Please enter the start year:"))
-    end_year = int(input("Please enter the ending year:"))
-    fragments = my_sub.split("-")
-
-    for year in range(start_year,end_year+1):
-        sub = sub_name + str(year)        
-        folder_location = "E:\\past_papers\\" + fragments[1]+"\\"+str(year)
-        if not os.path.exists(folder_location):
-            pathlib.Path(folder_location).mkdir(parents=True, exist_ok=True)
-        complete_url = url+"/"+my_sub+"/"+sub
-        print(complete_url)
-        response = requests.get(complete_url)
-        soup= BeautifulSoup(response.text, "html.parser")     
-        for link in soup.select("a[href$='.pdf']"):
-            #Name the pdf files using the last portion of each link which are unique in this case
-            filename = os.path.join(folder_location,link['href'].split('/')[-1])
-            with open(filename, 'wb') as f:
-                print("Current file:"+ filename)
-                f.write(requests.get(urljoin(url,link['href'])).content)
 
 if __name__ == "__main__":
-    #global url
-    #my_dic = form_dic()
-    #for _ in my_dic:
-    #    print(str(_)+"->"+my_dic[_])
-    #print("\n\n")
-    #print("Select the number of subject:")
-    #number = input()
-    #download_papers(my_dic[int(number)],url)
-    download_all_from_extreme_papers()
+    download_all()
     
     
---
+++ merge_pdfs.py
@@ -0,0 +1,182 @@
+import PyPDF2 as pypdf
+import textract
+import re
+import os
+import json
+
+def get_page_content(file_name,start_page_num=0,end_page_num=0):
+    with open(file_name, 'rb') as pdf:
+        pdf_file = pypdf.PdfFileReader(pdf)
+        total_pages = pdf_file.numPages
+        if not(0<=start_page_num<=total_pages and 0<=end_page_num<=total_pages ):
+            return None
+        else:
+            page_text=""
+            if start_page_num ==0 and end_page_num==0:
+                start_at = 1
+                end_at = total_pages
+            else:
+                start_at = start_page_num-1
+                end_at = end_page_num
+            for page_num in range(start_at,end_at):
+                page = pdf_file.getPage(page_num)
+                page_text+= page.extractText()
+            return page_text
+
+def split_pdf(file_name,start_page,end_page,output_file):
+    with open(file_name,"rb") as my_pdf:
+        pdf_file = pypdf.PdfFileReader(my_pdf)
+        total_pages = pdf_file.numPages
+        if not(0<=start_page<total_pages and 0<=end_page<total_pages and start_page<=end_page):
+            return None
+        else:
+            pdfWriter = pypdf.PdfFileWriter()
+            for page_num in range(start_page-1,end_page):
+                pdfWriter.addPage(pdf_file.getPage(page_num))
+            with open(output_file,"wb") as output_pdf:
+                pdfWriter.write(output_pdf)
+
+        
+def get_text_from_scanned_pdf(file_name):
+    extracted_text = textract.process(file_name, method='tesseract', language='eng')
+    return extracted_text
+
+def put_pages_to_text(mainfile,start_page_num,end_page_num):
+    split_pdf(mainfile,start_page_num,end_page_num,"temp.pdf")
+    my_text = get_text_from_scanned_pdf("temp.pdf")
+    os.remove("temp.pdf")
+    with open(mainfile+"_page_"+str(start_page_num)+"_to_"+str(end_page_num),"wb") as output_text:
+        output_text.write(my_text)
+
+def single_page_to_text(mainfile,page_num):
+    put_pages_to_text(mainfile,page_num,page_num)
+        
+        
+def parse_text_content(text_file):
+    with open(text_file,"r") as t_file:
+        texual_data = t_file.readlines()
+    data = {}
+    parent = ""
+    flag = 0
+    content = ""
+    regex_pattern = "^\d[\.|,]\s\w*"
+    for line in texual_data:
+        if flag==1 and re.match(regex_pattern,line):
+            flag=0
+            all_text = content.split("\n")
+            temp = ""
+            for i in range(len(all_text)-1):
+                temp+=all_text[i]
+                if all_text[i+1]=="" or all_text[i+1]==" ":
+                    if temp.strip()!="":
+                        data[parent].append(temp)
+                    temp = ""
+            content = ""
+        if re.match("\d+\.\d+.*",line):
+            for key in data:
+                if key.split(".")[0] == line.split(".")[0]:
+                    data[key].append(line.strip())
+        else:
+            content+=line
+        if flag==0 and re.match(regex_pattern,line):
+            flag=1
+            parent = line.strip().replace(",",".",1)
+            data[parent] = []
+            content = ""
+    return data
+
+def merge_similar_A_Levels_paper_into_one():
+    current_dir = os.getcwd()
+    data = ""
+    with open("subjects.json") as f:
+        data = json.load(f)
+    
+    all_dirs = [os.path.join(current_dir,sub) for sub in data["subjects"]]
+
+    paper_types_gathered = {}
+    
+    with open("errlog.txt","w") as o:
+        pass
+    for i in range(7):
+        paper_types_gathered[str(i)]=[]  
+
+    for dir in all_dirs:
+        for file_name in os.listdir(dir):
+            if file_name.endswith(".pdf"):
+                tokens = re.split("_|\.",file_name)
+                if len(tokens)==5:
+                    subject_code = tokens[0]
+                    paper_type = tokens[3][0]
+                    qp_or_ms = tokens[2]
+                    if qp_or_ms=="qp":
+                        if paper_type.isnumeric():
+                            paper_types_gathered[paper_type].append(file_name)
+
+
+        for key in paper_types_gathered:
+            pdfWriter = pypdf.PdfFileWriter()
+            paper_types_gathered[key].sort()
+            for item in paper_types_gathered[key]:
+                try:
+                    ms_of_current_paper = re.sub("qp","ms",item)
+                    qp_file_to_read = open(item,"rb")
+                    ms_file_to_read = open(ms_of_current_paper,"rb")
+                    pdf_qp_file = pypdf.PdfFileReader(qp_file_to_read)
+                    pdf_ms_file = pypdf.PdfFileReader(ms_file_to_read)
+                    total_pages = pdf_qp_file.numPages
+                    for page_num in range(0,total_pages):
+                        pdfWriter.addPage(pdf_qp_file.getPage(page_num))
+                    total_pages = pdf_ms_file.numPages
+                    for page_num in range(1,total_pages):
+                        pdfWriter.addPage(pdf_ms_file.getPage(page_num))
+                    print(f"Read from {item} successfully")
+                except:
+                    print(f"Failed to read content from {item}")
+                    with open("errlog.txt","a") as errlog:
+                        errlog.write(f"Failed to write  content for {item}\n")
+                        continue
+            write_file = open(f"{key}.pdf","wb")
+            pdfWriter.write(write_file)
+            write_file.close()
+            
+
+
+
+def run_program():
+    #put_pages_to_text("Chemistry_Syllabus.pdf",2,3)
+    #print(parse_text_content("Chemistry_Syllabus.pdf_page_2"))
+    #text = get_page_content("9701_s11_qp_11.pdf")
+    #questions = extract_a_levels_questions(text,"9701_m18_qp_12.pdf")
+    #for q_num,question in enumerate(questions):
+    #    print(q_num+1,question)
+    all_data = parse_text_content("chemistry_syllabus.txt")
+    print(all_data)
+    for key in all_data:
+        with open("managed_syllabus.txt","a") as write_file:
+            write_file.write(key+":\n")
+            print("Written:"+key)
+            for elements in all_data[key]:
+                write_file.write("\t"+elements+"\n")
+                print("Written:"+elements)
+    
+        
+def extract_a_levels_questions(text,file_name):
+    text = re.sub("(\w)\\n(\w)","\\1\\2",text)
+    #print(repr(text))
+    subject_code = file_name.split("_")[0]
+    regex_biology_P1 ="(?:\n\d+\n\s(\w.*?)\?)|(?:\n\d+\s\n(\w.*?)\?)"
+    regex_chemistry_P1 = "(?:\n\d+\s(\w.*?)\?)|(?:\n\[Turn\sover\s\d+\s(\w.*?)\?)|(?:\n\s\d+\s(\w.*?)\?)"
+    if subject_code=="9701":
+        regex_pattern = regex_chemistry_P1
+    elif subject_code=="9700":
+        regex_pattern = regex_biology_P1
+
+    return re.findall(regex_pattern,text,flags=re.DOTALL)
+
+
+if __name__ == '__main__':
+    merge_similar_A_Levels_paper_into_one()
+    
+
+
+
